054be2a0f119ee583426d715b6c93fbca6de3785
Taras Polovyi <taras.polovyi@grammarly.com>
2022-11-08 10:41:55 -0500

    rename basic example app

--- examples/basic/jest.config.ts
+++ examples/basic/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'basic-example',
+  displayName: 'basic',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
